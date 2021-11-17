676ae4d1812c58052fa4fcee222e61768b17146f
Jesus Riva <jesus.riva92@gmail.com>
2021-11-17 14:51:38 +0000

    se hicieron los modelos

---
+++ database/migrations/2021_11_17_133503_create_empresas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+	    $table->string('nombre');
+	    $table->string('logo');
+	    $table->string('color_primario');
+	    $table->string('color_secundario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
