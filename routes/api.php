db6dbe46b99915f8285e0a778080f7596e52ac4b
Chuy Riva <jesus.riva92@gmail.com>
2021-11-17 09:10:14 -0600

    login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\Usuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', function(Request $request) {
+	$credentials = $request->only('usuario', 'password');
+    return Usuario::where('usuario', $credentials->usuario)->where('password', $credentials->password)->first();
+});
+
