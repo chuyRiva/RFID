38f7a601553781c84f135c6430804de8288bb8be
Chuy Riva <jesus.riva92@gmail.com>
2021-11-17 12:40:41 -0600

    a

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login/', function(Request $request) {
 	$credentials = $request->only('usuario', 'password');
-    return Usuario::where('usuario', $credentials->usuario)->where('password', $credentials->password)->first();
+    return Usuario::where('usuario', $credentials['usuario'])->where('password', $credentials['password'])->first();
 });
 
 Route::get('/usuarios/', function() {
